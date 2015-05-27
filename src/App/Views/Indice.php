82051e393bc4589fe68328d4ce5a47ea6f6b12bd
Víctor Calzón <kung.funk@gmail.com>
2015-05-27 09:04:27 +0200

    Add: ejemplo para la memora Fix: usamos this para acceder a datos

--- src/App/Views/Indice.php
+++ src/App/Views/Indice.php
@@ -10,11 +10,9 @@ class Indice
 	public $data;
 
 	public function __construct(ModelsInterface $model)
-	{		
+	{
 		$this->model = $model;
-		var_dump($model);
 		$this->data = $this->model->getAllStats();
-		var_dump($data);
 		$this->printTemplate();
 	}
 
